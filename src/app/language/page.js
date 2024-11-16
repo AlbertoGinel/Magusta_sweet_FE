851a82db408ceb0449ac0d7baeb192b4e778ed28
Kristel Vainomets <kvaino@taltech.ee>
2024-11-16 16:38:48 +0200

    Language selection page. Need some work still

---
+++ src/app/language/page.js
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { useRouter } from 'next/navigation';
+
+export default function Page() {
+  const [selectedLanguage, setSelectedLanguage] = useState(null)
+  const router = useRouter();
+
+  const handleLanguageSelect = (language) => {
+    setSelectedLanguage(language)
+
+    if (language === 'English') {
+        router.push('/game');
+      } else if (language === 'Estonian') {
+        router.push('/game');
+      }
+  }
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl text-blue-500 font-bold mb-8">Please choose language to play</h1>
+      <div className="flex space-x-4 mb-8">
+        <Button
+          onClick={() => handleLanguageSelect('English')}
+          variant={selectedLanguage === 'English' ? 'default' : 'outline'}
+        >
+          English to Estonian
+        </Button>
+        <Button
+          onClick={() => handleLanguageSelect('Estonian')}
+          variant={selectedLanguage === 'Estonian' ? 'default' : 'outline'}
+        >
+          Estonian to English
+        </Button>
+      </div>
+      
+    </main>
+  )
+}
